4a268164f9a754fd1db51931dffb98ad1fb91768
mtpdevops <62241727+mtpdevops@users.noreply.github.com>
2020-04-22 00:14:09 +0300

    output "this_db_instance"

--- providers.tf
+++ providers.tf
@@ -16,3 +16,9 @@ terraform {
   # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
   required_version = ">= 0.12.0"
 }
+
+# output db instance resource
+output "this_db_instance" {
+  description = "The database resource"
+  value       = module.db_instance.this_db_instance
+}
